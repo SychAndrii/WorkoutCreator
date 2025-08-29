044a1db2c18980f84c6bb20354cade91f5ce3c97
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-08-28 21:05:20 -0400

    Validating length of exercise name

--- BaseDomain/NormalizedString.cs
+++ BaseDomain/NormalizedString.cs
@@ -19,6 +19,7 @@ namespace BaseDomain
             yield return _value;
         }
 
+        public int Length => _value.Length;
         public override string ToString() => _value;
 
         public static implicit operator string(NormalizedString val) => val?._value ?? string.Empty;
--- WorkoutDomain/ExerciseAggregate/Exercise.cs
+++ WorkoutDomain/ExerciseAggregate/Exercise.cs
@@ -1,4 +1,5 @@
-﻿using BaseDomain;
+﻿using System.Text.RegularExpressions;
+using BaseDomain;
 
 namespace WorkoutDomain.ExerciseAggregate
 {
@@ -11,12 +12,24 @@ namespace WorkoutDomain.ExerciseAggregate
         {
             Measurement.Duration
         });
+        private static readonly Regex ValidNameRegex = new(@"^[A-Za-z ]+$", RegexOptions.Compiled);
 
         public override NormalizedString Id => name;
 
         public Exercise(string name, ISet<Measurement> possibleMeasurements)
         {
-            this.name = name;
+            if (!ValidNameRegex.IsMatch(name))
+            {
+                throw new ArgumentException($"Invalid exercise name: [{name}]. Only English letters and spaces are allowed.");
+            }
+            NormalizedString normalizedName = name;
+
+            if (normalizedName.Length < 3)
+            {
+                throw new ArgumentException($"Invalid exercise name: [{name}]. The name must be at least 3 characters long.");
+            }
+            this.name = normalizedName;
+
             if (possibleMeasurements.Count == 0)
             {
                 throw new ArgumentException("There has to be at least one possible measurement for the exercise");
