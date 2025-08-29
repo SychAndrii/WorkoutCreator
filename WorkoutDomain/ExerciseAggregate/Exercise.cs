1f473450f697eee6b75aa602a32d50e35297a0fe
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-08-29 03:05:19 -0400

    Added new fields into the Exercise aggregate

--- WorkoutDomain/ExerciseAggregate/Exercise.cs
+++ WorkoutDomain/ExerciseAggregate/Exercise.cs
@@ -1,22 +1,50 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
 using BaseDomain;
+using WorkoutDomain.ExerciseAggregate.Enums;
 
 namespace WorkoutDomain.ExerciseAggregate
 {
     public class Exercise : AggregateRoot<NormalizedString>
     {
-        public NormalizedString name;
-        private readonly ISet<Measurement> possibleMeasurements;
+        public NormalizedString Name
+        {
+            get;
+        }
+        public Difficulty Difficulty
+        {
+            get;
+        }
+        public ExerciseCategory Category
+        {
+            get;
+        }
+        public IReadOnlySet<Measurement> PossibleMeasurements
+        {
+            get;
+        }
+        public IReadOnlySet<Equipment>? RequiredEquipment
+        {
+            get;
+        }
+        public IReadOnlySet<MuscleGroup>? PrimaryMuscles
+        {
+            get;
+        }
+        public IReadOnlySet<MuscleGroup>? SecondaryMuscles
+        {
+            get;
+        }
 
-        public readonly static Exercise REST = new("Rest", new HashSet<Measurement>
+        public readonly static Exercise REST = new("Rest", Difficulty.Low, ExerciseCategory.Recovery, new HashSet<Measurement>
         {
             Measurement.Duration
-        });
+        }, null, null, null);
         private static readonly Regex ValidNameRegex = new(@"^[A-Za-z ]+$", RegexOptions.Compiled);
 
-        public override NormalizedString Id => name;
+        public override NormalizedString Id => Name;
 
-        public Exercise(string name, ISet<Measurement> possibleMeasurements)
+        private Exercise(string name, Difficulty difficulty, ExerciseCategory category, ISet<Measurement> possibleMeasurements, ISet<Equipment>? equipment, ISet<MuscleGroup>? primaryMuscles, ISet<MuscleGroup>? secondaryMuscles)
         {
             if (!ValidNameRegex.IsMatch(name))
             {
@@ -28,18 +56,53 @@ namespace WorkoutDomain.ExerciseAggregate
             {
                 throw new ArgumentException($"Invalid exercise name: [{name}]. The name must be at least 3 characters long.");
             }
-            this.name = normalizedName;
+
+            Name = normalizedName;
+            Difficulty = difficulty;
+            Category = category;
 
             if (possibleMeasurements.Count == 0)
             {
                 throw new ArgumentException("There has to be at least one possible measurement for the exercise");
             }
-            this.possibleMeasurements = possibleMeasurements;
-        }
+            PossibleMeasurements = possibleMeasurements.ToImmutableHashSet();
 
-        public bool AllowsMeasurement(Measurement type)
-        {
-            return possibleMeasurements.Contains(type);
+            if (equipment != null && equipment.Count == 0)
+            {
+                throw new ArgumentException("Required equipment must either be not specified at all or be non-empty");
+            }
+            RequiredEquipment = equipment?.ToImmutableHashSet();
+
+            if (primaryMuscles != null && primaryMuscles.Count == 0)
+            {
+                throw new ArgumentException("Primary muscle groups must either be not specified at all or be non-empty");
+            }
+            PrimaryMuscles = primaryMuscles?.ToImmutableHashSet();
+
+            if (secondaryMuscles != null && secondaryMuscles.Count == 0)
+            {
+                throw new ArgumentException("Secondary muscle groups must either be not specified at all or be non-empty");
+            }
+            SecondaryMuscles = secondaryMuscles?.ToImmutableHashSet();
+
+            if (PrimaryMuscles == null && SecondaryMuscles != null)
+            {
+                throw new ArgumentException("Secondary muscle groups must be specified only if Primary muscle groups are also specified");
+            }
+
+            if (PrimaryMuscles != null && SecondaryMuscles != null)
+            {
+                var samePrimaryAndSecondaryMuscles = PrimaryMuscles.Intersect(SecondaryMuscles);
+                if (samePrimaryAndSecondaryMuscles.Any())
+                {
+                    throw new ArgumentException($"Primary muscle groups must not also be secondary muscle groups. Repeated primary and secondary muscles: [{samePrimaryAndSecondaryMuscles}]");
+                }
+            }
+
+            if ((Category == ExerciseCategory.Strength || Category == ExerciseCategory.Cardio) && PrimaryMuscles == null)
+            {
+                throw new Exception("Strength and Cardio exercises must have at least one primary muscle group targeted");
+            }
         }
     }
 }
