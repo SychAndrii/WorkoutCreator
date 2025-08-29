e3f93775c0f97c0618cb50388324e2f524a5aa48
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-08-28 21:24:24 -0400

    Added ExerciseCategory

---
+++ WorkoutDomain/ExerciseAggregate/ExerciseCategory.cs
@@ -0,0 +1,19 @@
+﻿using BaseDomain;
+
+namespace WorkoutDomain.ExerciseAggregate
+{
+    public sealed class ExerciseCategory : Enumeration
+    {
+        public static readonly ExerciseCategory Strength = new("STRENGTH");
+        public static readonly ExerciseCategory Cardio = new("CARDIO");
+        public static readonly ExerciseCategory Mobility = new("MOBILITY");
+        public static readonly ExerciseCategory Recovery = new("RECOVERY");
+        public static readonly ExerciseCategory Breathing = new("BREATHING");
+
+        private ExerciseCategory(string name) : base(name) { }
+
+        public static IEnumerable<ExerciseCategory> List() => GetAll<ExerciseCategory>();
+        public static ExerciseCategory FromString(string name) => FromString<ExerciseCategory>(name);
+    }
+
+}
