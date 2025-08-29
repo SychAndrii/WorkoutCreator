11ca312c50ea325d638d359c5f648224cc75b841
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-08-29 10:40:47 -0400

    Added documentation to the Difficulty class

--- WorkoutDomain/ExerciseAggregate/Enums/Difficulty.cs
+++ WorkoutDomain/ExerciseAggregate/Enums/Difficulty.cs
@@ -2,6 +2,14 @@
 
 namespace WorkoutDomain.ExerciseAggregate.Enums
 {
+    /// <summary>
+    /// Represents the difficulty level of an exercise.
+    /// </summary>
+    /// <remarks>
+    /// This is a strongly typed enumeration based on the <see cref="Enumeration"/> base class.
+    /// It allows associating string identifiers with difficulty levels and provides useful utilities
+    /// for parsing and listing defined values.
+    /// </remarks>
     public sealed class Difficulty : Enumeration
     {
         public static readonly Difficulty Low = new("LOW");
@@ -10,7 +18,18 @@ namespace WorkoutDomain.ExerciseAggregate.Enums
 
         private Difficulty(string name) : base(name) { }
 
+        /// <summary>
+        /// Returns all defined <see cref="Difficulty"/> values.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerable{Difficulty}"/> containing all defined difficulties.</returns>
         public static IEnumerable<Difficulty> List() => GetAll<Difficulty>();
+
+        /// <summary>
+        /// Parses a string into a <see cref="Difficulty"/> instance.
+        /// </summary>
+        /// <param name="name">The name of the difficulty level to parse.</param>
+        /// <returns>The corresponding <see cref="Difficulty"/> instance.</returns>
+        /// <exception cref="ArgumentException">Thrown if the name does not match any defined difficulty.</exception>
         public static Difficulty FromString(string name) => FromString<Difficulty>(name);
     }
 }
